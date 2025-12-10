c7252b2b60f38ed28b193943f4ec5a214f3da376
Matheus Brito <matheuswnb@kali.kaliwnb>
2025-12-10 19:18:27 -0300

    Tentando escrever no array e printar a mensagem, está quebrado

--- Pratica_2/pratica_2.asm
+++ Pratica_2/pratica_2.asm
@@ -1,19 +1,22 @@
 section .data
-    array: db '10', 0x0a,'20', 0x0a,'30', 0x0a,'40', 0x0a
-    ; pular_linha: db 0xa
+    array: db '00', 0x0a,'03', 0x0a,'06', 0x0a,'09', '' 0x0a
+    tamanho_array: equ $- array
+
+    mensagem_escrita: db "Escreva 3 caracteres: "
+    tamanho_escrita: equ $- mensagem_escrita
 
 section .text
     global _start
 
 _start:
-    mov rcx, 12 ;Quantidade de bytes que serão iterados no array
+    mov rcx, tamanho_array ;Quantidade de bytes que serão iterados no array
     mov rax, 0 ;Armazena o valor de rdx com os bytes que serão iterados
     mov rdi, 1
     mov rdx, 0
    
 .loop:
     add rax, rdx 
-    lea rsi, [array + rax] ;Rsi obtém o endereço do primeiro elemento do array
+    lea rsi, [array + rax] ;Rsi obtém o endereço do primeiro elemento do array + rax
 
     sub rcx, 3
     add rdx, 3
@@ -29,7 +32,10 @@ _start:
     pop rcx 
     pop rax
 
-    ;Se as interações não acabaram continua no loop
+    cmp rdx, 12
+    je .escrever_no_array
+
+    ;Se as iterações não acabaram continua no loop
     cmp rcx, 0 
     jnz .loop
 
@@ -37,6 +43,41 @@ _start:
     mov rax, 60
     xor rdi, rdi
     syscall
+
+.escrever_no_array:
+    push rax
+    push rcx
+    push rdx
+    push rsi
+
+    mov rax, 1 
+    mov rdi, 1
+    mov rdx, tamanho_escrita
+    mov rsi, mensagem_escrita
+    syscall
+
+    mov rax, 0 ;Código syscall read
+    mov rdi, 0 
+    lea rsi, [array + 12]
+    mov rdx, 2
+    syscall
+
+    pop rsi
+    pop rdx
+    pop rcx
+    pop rax
+
+    call .loop
+
+
+    
+    
+
+
+
+
+
+
 
 
 
