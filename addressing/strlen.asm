6a8a58b1b47919770abd9e66b27e5e506a7e364e
Matheus Brito <matheuswnb@kali.kaliwnb>
2025-12-28 20:18:22 -0300

    fix: 'rax' is saving the exactly lenght  of 'string' and exiting code

--- addressing/strlen.asm
+++ addressing/strlen.asm
@@ -11,18 +11,33 @@ loop:
     cmp byte[rdi + rax], 0
     je end
 
+    lea rsi, [rdi + rax]
+
+    push rax
+    push rcx
+    push rdi
+
+    mov rax, 1
+    mov rdi, 1
+    mov rdx, 1
+    syscall
+
+    pop rdi
+    pop rcx
+    pop rax
+
     inc rax
 
     jmp loop
 
 _start:
-    mov rdi, [rel string]
+    lea rdi, [rel string]
     mov rax, 0
 
     call loop
 
     mov rbx, rdi
 
-    mov rax, 1
+    mov rax, 60
     xor rdi, rdi
     syscall
