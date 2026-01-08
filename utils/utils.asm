4070f0a4a1997b6d82eef5ed908d298644345cb3
Matheus WNB <matheuswnb@WNB.wnb>
2026-01-08 02:03:33 -0300

    Update: utils

--- utils/utils.asm
+++ utils/utils.asm
@@ -7,7 +7,7 @@
     syscall
 %endmacro
 
-;Push registers
+;Print string
 %macro sys_print 2
     mov rax, 1
     mov rdi, 1
@@ -15,5 +15,35 @@
     mov rsi, %2
     syscall
 %endmacro
+
+%macro push_registers 3
+    push %1
+    push %2
+    push %3
+%endmacro
+
+%macro pop_registers 3
+    pop %1
+    pop %2
+    pop %3
+%endmacro
+
+section .text
+    global loop_1
+
+loop_1:
+    cmp byte[rdi + rax], 0
+    je end
+
+    lea rsi, [rdi + rax]
+
+    push_registers rax, rcx, rdi
+
+    sys_print 1, rsi
+
+    pop_registers rdi, rcx, rax
+
+    inc rax
+    jmp loop_1
 
 
